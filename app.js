e01a2df3894626afdd996adf29aa60ea42975a9d
Kartal Gules <kartalgules@gmail.com>
2023-06-03 04:40:37 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const fn = function () {
-    console.log('working')
-}
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
 
-fn()
+app.use(express.static('public'))
+
+app.get('/', (req,res)=>{
+
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+})
+
+app.listen(port, ()=>{
+    console.log(`Sunucu ${port} portunda başlatıldı..`)
+})
