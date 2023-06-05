c6e6975840f0eb7fb76ed6279267d34f1972f82b
Kartal Gules <kartalgules@gmail.com>
2023-06-06 02:34:43 +0300

    all files updates added

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const ejs = require('ejs')
 const port = 3000
 
+// Template Engine
+app.set("view engine", "ejs")
+
+//  Middlewares
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
+//  Routes
 app.get('/', (req,res)=>{
+    res.render('index')
+})
+app.get('/about', (req,res)=>{
+    res.render('about')
+})
+app.get('/add', (req,res)=>{
+    res.render('add')
+})
 
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+app.post('/photos', (req,res)=>{
+    console.log(req.body);
+    res.redirect('/')
 })
 
 app.listen(port, ()=>{
